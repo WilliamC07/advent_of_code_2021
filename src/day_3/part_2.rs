1e6136b7a5047a184e5b7e763fa91d4853d8a475
willcao <willcao@umich.edu>
2021-12-20 21:07:24 -0500

    finished day 3

---
+++ src/day_3/part_2.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+
+pub fn run() {
+    println!("Running Day 3 part 2:");
+
+    let path = Path::new("src/day_3/input_part2.txt");
+    let display = path.display();
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("Can't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let lines = io::BufReader::new(file).lines();
+
+    const NUM_BITS: u32 = 12;
+    let mut occurrences_of_one: [u32; NUM_BITS as usize] = [0; NUM_BITS as usize];
+
+    for line in lines {
+        if let Ok(bitstring) = line {
+            for (position, value) in bitstring.chars().enumerate() {
+                occurrences_of_one[position] += (value as u32) - '0' as u32;
+            }
+        }
+    }
+
+    let epsilon =
+        occurrences_of_one
+            .iter()
+            .map(|val| if *val > 500 {0} else {1})
+            .map(|val| val.to_string())
+            .collect::<String>(); // least common
+    let epsilon = i32::from_str_radix(&epsilon, 2).unwrap();
+    let gamma =
+        occurrences_of_one
+            .iter()
+            .map(|val| if *val > 500 {1} else {0})
+            .map(|val| val.to_string())
+            .collect::<String>(); // most common
+    let gamma = i32::from_str_radix(&gamma, 2).unwrap();
+
+    println!("Answer day 3 part 2: {}", gamma * epsilon);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
-    day_2::part_1::run();
-    day_2::part_2::run();
+    day_3::part_1::run();
+    day_3::part_2::run();
 }
